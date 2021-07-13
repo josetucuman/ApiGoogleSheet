28224658e9b24503b7194e6d592298ec7c1189d7
Jose <josegelimergomez@gmail.com>
2021-07-13 19:35:25 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button, Form, Container, Header } from 'semantic-ui-react'
 
-function App() {
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container fluid className="container">
+        <Header as='h2'>React Google Sheets</Header>
+        <Form className="form">
+          <Form.Field>
+            <label>Nombre</label>
+            <input placeholder="Ingrese su nombre" />
+          </Form.Field>
+          <Form.Field>
+            <label>Edad</label>
+            <input placeholder="Ingrese su edad" />
+          </Form.Field>
+          
+        </Form>
+      </Container>
     </div>
-  );
+  )
 }
-
-export default App;
